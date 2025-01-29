cabf03a4bbe7bfd692e8f9bfaf2c0021effb4cc3
SudhangshuBaruah001 <156283313+SudhangshuBaruah001@users.noreply.github.com>
2025-01-29 19:57:59 +0530

    Add files via upload

---
+++ Calculate the area of a circle.py
@@ -0,0 +1,4 @@
+import math
+radius = float(input("Enter the radius: "))
+Area = math.pi * radius ** 2
+print("Area of the circle:", Area)
---
+++ Find the largest item from a given list.py
@@ -0,0 +1,2 @@
+lst = [10, 20, 55, 88,90 ]
+print("Largest number:", max(lst))
